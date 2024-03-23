1f7ebb2865cb75942712e90c5a4a74b7a0b5eb9e
Igor Rosa <igorrosafilgueiras@gmail.com>
2024-03-23 10:52:58 -0300

    init commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# Creation_Pattern_Factory_Method
---
+++ src/main/java/org/example/ServiceFactory.java
@@ -0,0 +1,20 @@
+package org.example;
+
+public class ServicoFactory {
+
+    public static IService service(String service) {
+        Class aClass = null;
+        Object object = null;
+        try{
+            aClass = Class.forName("org.example.Service"+service);
+            object = aClass.newInstance();
+        }catch (Exception e) {
+            throw new IllegalArgumentException("non-existent service");
+        }
+        if (!(object instanceof IService)) {
+            throw new IllegalArgumentException("Invalid Service");
+        }
+        return (IService) object;
+    }
+
+}
